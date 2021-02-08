f5d3a37256f2fa1d91e2151a771b8f92a7fa1312
BitastaB <bitasta.b@gmail.com>
2021-02-08 02:42:30 +0100

    determined moving objects

--- bulb-analysis.py
+++ bulb-analysis.py
@@ -12,17 +12,18 @@ class SceneObject:
       print(f"Initialising {name}")
       self.name = name 
       self._location = []
+      self._moving = False
       print("initalisation complete")
     
  # @property
   def get_location(self,i):
       """I'm the location of the scene object."""
-      print(f"getter of location called for {self.name} for frame {i}")
+     # print(f"getter of location called for {self.name} for frame {i}")
       return self._location[i]
 
  # @loc.setter
   def set_location(self, loc):
-      print(f"setter of location called for {self.name}")
+   #   print(f"setter of location called for {self.name}")
    #   print("Length of frame_and_loc: "+ str(len(frame_and_loc)))
     #  print("frame no.: "+ str(frame_and_loc[0]))
     #  print("location of object : "+ str(frame_and_loc[1]))
@@ -32,7 +33,10 @@ class SceneObject:
       
   def get_by_name(self, name):
       if(self.name == name):
-          return self     
+          return self    
+
+  def set_moving(self, isMoving):
+       self._moving = isMoving
     
 
 
@@ -40,26 +44,25 @@ class SceneObject:
 def set_scene_properties():
     
     i=100
-    print("Setting scene properties")
+    print("\nSetting scene properties")
     for obj in bpy.context.scene.objects:
-      if obj.type == 'MESH': 
+      if obj.type == 'MESH':
         so = SceneObject(obj.name)
-        print("added object : "+so.name)
         scene_objects_dict[obj.name] = so
         print(f"Setting params completed for {obj.name}\n")
         
     print("Total objects in current scene : " + str(len(scene_objects_dict)))
+
                   
-########### Process location/rotation of objects during animation 
-               
-def process_obj_location(scene):
-    print("HAHAHAHAHAAHAHAHA")
+########### Process location/rotation of objects during animation                
+def process_obj_location(scene):    
     if scene.frame_current in frame_read_counter:
         return
-    print(f"Processing object location for frame : {str(scene.frame_current)}")   
+    print(f"\nProcessing all object locations for frame : {str(scene.frame_current)}") 
     for obj in bpy.context.scene.objects:
+    #for obj in scene.objects:
       if obj.type == 'MESH': 
-          location = obj.rotation_euler
+          location = obj.matrix_world@obj.location
           object_instance = scene_objects_dict.get(obj.name, None)
           object_instance.set_location(location)
     
@@ -70,12 +73,35 @@ def process_obj_location(scene):
  
 ########## Analyse object movement post animation   
 def analyse_scene():
-    print("Animation over, will analyse scene now")
-    ladder_object = scene_objects_dict.get("Ladder")
-    print("Testing accessing Ladder locations 0 : "+ str(ladder_object.get_location(0)) + " 50 : "+str(ladder_object.get_location(50)))  
-    p_object = scene_objects_dict.get("Pinnochio")
-    print("Testing accessing Pinnochio locations 0 : "+ str(p_object.get_location(0)) + " 50 : "+str(p_object.get_location(50)))  
-
+    print("\nAnimation over, will analyse scene now")
+    
+    move_change_counter = {}
+    #Loop over all frames to find if position changes for each object
+    frame_range = bpy.data.scenes["Scene"].frame_end
+    for i in range(1, frame_range):
+        for name, scene_object in scene_objects_dict.items():   
+            #Ignoring Room objects to avoid complications      
+            if name == 'Room':
+                continue
+            #Set move tracker counter to 0 at the first frame
+            curr_obj_loc = scene_object.get_location(i)
+            prev_obj_loc = scene_object.get_location(i-1)
+            if name not in move_change_counter:
+                move_change_counter[name] = 0
+            if (curr_obj_loc.x - prev_obj_loc.x != 0) or (curr_obj_loc.y - prev_obj_loc.y != 0) or curr_obj_loc.z - prev_obj_loc.z != 0:
+                move_change_counter[name]+= 1
+                
+    #Loop of objects to check if position changes for more than 90% of the total no. of frames
+    #if yes set moving = True
+    for name, move_counter in move_change_counter.items():
+        if move_counter >= 0.9*frame_range:
+            scene_object = scene_objects_dict.get(name, None)
+            scene_object.set_moving(True)
+            print(f"{name} is rotating atleast 90% of the time")
+        else:
+            print(f"{name} is not rotating atleast 90% of the time") 
+            
+    print("Finished analysing scene")
     
         
 #################### code to play animation #######################################################################
@@ -119,6 +145,7 @@ class ModalTimerOperator(bpy.types.Operator):
 
 def register():
     bpy.utils.register_class(ModalTimerOperator)
+    bpy.app.handlers.frame_change_post.append(process_obj_location)
 
 
 def unregister():
@@ -135,5 +162,7 @@ if __name__ == "__main__":
 
     # test call
     bpy.ops.wm.modal_timer_operator()
-    bpy.app.handlers.frame_change_post.append(process_obj_location)
+
+    
+    
     
