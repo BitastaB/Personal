abb83366a9b89e1e2b4f6c52301232ed05a8f3b9
BitastaB <bitasta.b@gmail.com>
2021-05-23 00:06:19 +0200

    latest updates

--- RL-try1.py
+++ RL-try1.py
@@ -7,14 +7,16 @@ env = gym.make('Taxi-v3')
 
 def train():
     print("training")
-    q = np.full((env.observation_space.n, env.action_space.n), -0.0001)
+    q = np.full((env.observation_space.n, env.action_space.n), -0.0015)
     gamma = 0.8
     delta = 0.8
-    for t in range(1, 3000):
+    for t in range(1, 15000):
         steps = 0
         done = False
         current_state = env.reset()  # initial state
         print("starting new episode")
+        print("initial state:\n")
+        env.render()
         while not done:
             episode_reward = 0
            # print("current state : ", current_state)
@@ -34,29 +36,39 @@ def train():
             steps += 1
             episode_reward += reward
           #  print(f"step {steps} finished\nis it done? {done}\n")
-            if done:
-                print("total reward for this episode", episode_reward)
 
         print(f"episode converged in {steps} steps")
+        print("total reward for this episode", episode_reward)
+        print("final state")
+        env.render()
+        print("==============================================================================")
     return q
 
 
 Q = train()
 
 def test():
-    print("testing")
-    current_state = env.reset()
-    done = False
-    rewards = 0
-    steps = 0
-    while not done:
-        env.render()
-        action = np.argmax(Q[current_state])
-        next_state, reward, done, info = env.step(action)
-        current_state = next_state
-        steps += 1
-        rewards += reward
-    print(f"converged with {steps} steps and {rewards} reward")
+    print("* * * * * * * STARTING testing * * * * * * * ")
+    n = 10
+    total_mistakes = 0
+    for passenger in range(n):
+        current_state = env.reset()
+        done = False
+        rewards, steps, mistakes = 0, 0, 0
+        print(f"passenger {passenger+1} starting ...")
+        while not done:
+            env.render()
+            action = np.argmax(Q[current_state])
+            next_state, reward, done, info = env.step(action)
+            current_state = next_state
+            if reward == -10:
+                mistakes += 1
+            steps += 1
+            rewards += reward
+        total_mistakes += mistakes
+        print(f"passenger {passenger+1} converged with {steps} steps and {rewards} reward and {mistakes} mistakes")
+        print("=========================================================================================================")
+    print("total mistakes", total_mistakes/n)
 
 
 test()
